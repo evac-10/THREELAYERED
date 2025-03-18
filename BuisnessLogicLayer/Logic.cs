2362182e5b317b009034bf0d91afd5ffdcc5a253
evac-10 <160251360+evac-10@users.noreply.github.com>
2025-03-18 23:37:40 +0500

    Made three layered architecture with database

---
+++ BuisnessLogicLayer/Logic.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace BuisnessLogicLayer
+{
+    public class Logic
+    {
+        private Data studentDAL = new Data();
+
+        public DataTable GetAllStudents()
+        {
+            return studentDAL.GetStudents();
+        }
+    }
+}
---
+++ DataAccessLayer/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAccessLayer
+{
+    
+    public class Data
+    {
+        private string connectionString = "Server=DESKTOP-KI3V701\\SQLEXPRESS;Database=Test;Trusted_Connection=True;";
+        public DataTable GetStudents()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT StudentName FROM Students";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    con.Open();
+                    da.Fill(dt);
+                }
+            }
+            return dt;
+        }
+    }
+}
